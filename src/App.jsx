3dc38b2a248c2f29c248ba5fde84e8e1f2ce6fb7
siriwatknp <siriwatkunaporn@gmail.com>
2024-01-13 14:49:49 +0700

    finish basic req 1: create basic markup for the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,50 @@
 function App() {
-  return <div style={{ maxWidth: 400, margin: "auto" }}>Hello</div>;
+  return (
+    <div style={{ maxWidth: 400, margin: "auto" }}>
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <h1 style={{ flex: "auto" }}>Authenticator</h1>
+        <button
+          style={{
+            display: "flex",
+            backgroundColor: "violet",
+            color: "#fff",
+            border: "none",
+            padding: "0.5rem 1rem",
+            fontSize: "1rem",
+            fontWeight: 500,
+            borderRadius: "4px",
+            cursor: "pointer",
+          }}
+        >
+          New
+        </button>
+      </div>
+      <ul
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          margin: 0,
+          padding: 0,
+          listStyle: "none",
+        }}
+      >
+        {[...Array(10)].map((_, i) => (
+          <li
+            key={i}
+            style={{
+              borderBottom: "1px solid rgba(0 0 0 / 0.12)",
+              paddingBlock: "0.5rem",
+            }}
+          >
+            <div style={{ fontSize: "1.25rem" }}>Google</div>
+            <div style={{ fontSize: "1.5rem", color: "blueviolet" }}>
+              181 951
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
