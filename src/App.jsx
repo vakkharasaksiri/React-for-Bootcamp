0bd5aebba4966f0b52f88f571e06a7d59420f8ec
siriwatknp <siriwatkunaporn@gmail.com>
2024-01-24 20:58:14 +0700

    add guide for basic req 4

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ function App() {
   const [countries, setCountries] = useState([]);
   const [text, setText] = useState("");
   const [loading, setLoading] = useState(false);
+  // 1. create a state named `language` with initial value `eng`
   useEffect(() => {
     async function fetchData() {
       setLoading(true);
@@ -29,6 +30,8 @@ function App() {
         "loading..."
       ) : (
         <>
+          {/* 2. create a <select> with these options: `eng, ara, fra, ita, jpn, zho`. style it as you'd like */}
+          {/* 3. bind the language state with the <select> */}
           <input
             placeholder="Type to find..."
             value={text}
@@ -46,6 +49,8 @@ function App() {
           <ul style={{ margin: 0, padding: 0, marginTop: "1rem" }}>
             {countries
               .filter((country) => country.name.official.includes(text))
+              // 4. filter the countries by the selected language
+              //    💡 HINT: use the `translations` property of the country object
               .map((country) => (
                 <li
                   key={country.name.official}
@@ -56,6 +61,7 @@ function App() {
                   }}
                 >
                   {country.flag} {country.name.official}
+                  {/* 5. show the translated name if possible. */}
                 </li>
               ))}
           </ul>
