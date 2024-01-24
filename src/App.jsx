a3d37e8f79d37a0c45f11284a970693ef970e34b
siriwatknp <siriwatkunaporn@gmail.com>
2024-01-24 21:34:45 +0700

    add guide for basic req 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
 function App() {
-  return <div style={{ maxWidth: 400, margin: "auto" }}>Hello</div>;
+  return (
+    <div style={{ maxWidth: 400, margin: "auto" }}>
+      Hello
+      {/* 1. render a <h1> with a title */}
+      {/* 2. render a <button> for creating a new account */}
+      {/* 3. render a list of accounts with hardcoded data
+         💡 TIP: you can use `[...Array(10)]` to create array of 10 items
+       */}
+    </div>
+  );
 }
 
 export default App;
