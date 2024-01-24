240178292f2321b80439950aeb23de7acf74fb4c
siriwatknp <siriwatkunaporn@gmail.com>
2024-01-24 20:41:58 +0700

    add guide for basic req 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
 function App() {
   return (
-    <main style={{ maxWidth: 400, margin: "auto" }}>Add content here.</main>
+    <main style={{ maxWidth: 400, margin: "auto" }}>
+      Add content here.
+      {/* 1. render a <h1> with a title here. */}
+      {/* 2. render an <input> with a placeholder here. */}
+      {/* 3. using <ul> and <li> to render a list of countries [
+          { name: "United States", flag: "🇺🇸" },
+          { name: "Brazil", flag: "🇧🇷" },
+          { name: "Thailand", flag: "🇹🇭" },
+          { name: "Japan", flag: "🇯🇵" },
+          { name: "China", flag: "🇨🇳" },
+        ] */}
+    </main>
   );
 }
 
