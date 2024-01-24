b125fdcd7308229dbd83def39a4418adbaa07349
siriwatknp <siriwatkunaporn@gmail.com>
2024-01-24 20:53:29 +0700

    add guide for basic req 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import { useState } from "react";
 
 function App() {
+  // 1. create a state variable named `countries` to store the countries as array.
   const [text, setText] = useState("");
+  // 2. create a state variable named `loading` to store the loading state as boolean.
+
+  // 3. create a useEffect to fetch the countries from the API.
+  //    💡 TIP: open "https://restcountries.com/v3.1/all" to see the data in your browser
+  //    💡 TIP: use `fetch` API to fetch the data.
+
   return (
     <main
       style={{
@@ -10,6 +17,7 @@ function App() {
       }}
     >
       <h1>Country Finder</h1>
+      {/* 4. render "loading..." or an icon when the `loading` state is true otherwise, render the input and countries. */}
       <input
         placeholder="Type to find..."
         value={text}
@@ -25,6 +33,9 @@ function App() {
         }}
       />
       <ul style={{ margin: 0, padding: 0, marginTop: "1rem" }}>
+        {/* 5. replace the mockup data with the `countries` from the API
+               and make sure to use the right properties.
+        */}
         {[
           { name: "United States", flag: "🇺🇸" },
           { name: "Brazil", flag: "🇧🇷" },
